1781838bb6dcdd23b1548844d2c71bd7a6bb96f6
Matakoyako12 <123723699+Matakoyako12@users.noreply.github.com>
2024-07-11 19:37:13 +0300

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+oconst fs = require('fs');
 const chalk = require('chalk');
 
 
@@ -7,24 +7,24 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
+global.ownername = "Ray thee bandit";
+global.owner = ['254791195553'];
 global.premium = ['917050906659'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "Ray thee-bandit";
 global.sessionName = 'session';
-global.prefa = [".", "!", "/", "?"];
+global.prefa = ["."];
 global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
-global.autoread = false
+global.autoread = true
 global.available = false
 global.autoTyping = false
 global.autoRecord = false
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
-global.welcome = true
+global.welcome = false
 global.autoBlock = true
  
 /*REPLY MESSAGE*/
@@ -32,9 +32,9 @@ global.mess = {
     banned: '`You are *Banned* fron using commands!`',
     bangc: '`This Group is *Banned* from using Commands!`',
     success: '`✓ Success`',
-    admin: '`This feature is only for group admins`',
+    admin: '`ask your father maybe cuz i dont see you crowned`',
     botAdmin: '`I am not an admin!`',
-    owner: '`You are not my owner`',
+    owner: '`better ask ray`',
     group: '`You can use this command only in groups ❌`',
     private: '`Feature is used only for private chats!`',
     bot: '`Bot number user special features`',
